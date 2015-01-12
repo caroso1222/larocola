b0a24ecad64e7dc3bb8abae9a7c1724299a58a3f
caroso1222 <ce.roso398@gmail.com>
2015-01-12 18:29:55 -0500

    Add setting.py to .gitignore to pull from webfaction

--- songs/views.py
+++ songs/views.py
@@ -32,9 +32,9 @@ def videos(request,year):
 		len_author = len(rand_song.author)
 		len_name = len(rand_song.name)
 
-		if len_author > 42 and len_name > 20:
+		if len_author > 42 and len_name > 22:
 			box_height = 'big_x2'
-		elif len_name > 20:
+		elif len_name > 22:
 			box_height = 'big_x1'
 		elif len_author > 42:
 			box_height = 'big_x0'
@@ -49,4 +49,3 @@ def videos(request,year):
 		output = ":( ese año no existe"
 		print traceback.format_exc()
 		return HttpResponse(output)
-
