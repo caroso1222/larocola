4753511116cd6c8dce6e085f5a376f726bc8e630
caroso1222 <ce.roso398@gmail.com>
2015-01-16 01:00:22 -0500

    Change button Dame otra to send a POST request with the current song

--- songs/views.py
+++ songs/views.py
@@ -33,30 +33,36 @@ def videos(request,year):
 	visitor.counter_videos = F('counter_videos')+1
 	visitor.save()
 
+	#Saves the previous song in the last_visited FIFO list
+	ultimas = visitor.last_visited.split('?')
+	if request.method == 'POST':
+		previous_song = request.POST.get('previous_song','')
+		ultimas[0]=ultimas[1]
+		ultimas[1]=ultimas[2]
+		ultimas[2]=previous_song
+		visitor.last_visited='?'.join(ultimas)
+		visitor.save()
+		print visitor.last_visited
 
 	try:
 		songs = Song.objects.filter(year__year = year)
 		count = songs.all().count()
-		random_index = randint(0,count-1)
-		if request.method == 'POST':
-			previous_song = request.POST.get('previous_song','')
-			ultimas = visitor.last_visited.split('?')
-			print ultimas
-			ultimas[0]=ultimas[1]
-			ultimas[1]=ultimas[2]
-			ultimas[2]=previous_song
-			visitor.last_visited='?'.join(ultimas)
-			visitor.save()
-			print visitor.last_visited
-		rand_song = songs[random_index]
+
+		#Selects a random song that's not in the last_visited list. 
+		#This prevents that the next song is one of the last 3 songs.
+		stop = False
+		while stop != True:
+			random_index = randint(0,count-1)
+			rand_song = songs[random_index]
+			url_code = rand_song.youtube_url.split('=')[1]
+			if(url_code!=ultimas[0] and url_code!=ultimas[1] and url_code!=ultimas[2]):
+				stop = True
 
 		context = {"song": rand_song}
-		url_code = rand_song.youtube_url.split('=')[1]
 		#url_init = "//www.youtube.com/embed/"
 		#autoplay = "?autoplay=1"
 		#final_url = url_init+url_code+autoplay
 		context['url']=url_code
-		context['year']=year
 		template = 'year.html'
 
 
